2d1cf3387c6094eea5024f6eb263c7b5bcdb6ef2
Tony Pham <tnpham@valdosta.edu>
2022-05-20 15:14:38 -0400

    commit tree b12f90bae5df9231d86169d04acb75ea36f66d85

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.body.remove('main#main');
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Levi is the champion";
+document.body.append(newHeader);
